1e7f7ee25866a89875bfb9887fedd4b90626fab6
BaubasDRSK <127085633+BaubasDRSK@users.noreply.github.com>
2023-05-17 00:50:58 +0300

    grid

--- src/components/addnew.jsx
+++ src/components/addnew.jsx
@@ -7,22 +7,46 @@ export default function AddNew({ setCreateData }){
     const nameRef = useRef(null);
     const surnameRef = useRef(null);
 
+    const [nameError, setNameError] = useState('');
+    const [surnameError, setSurnameError] = useState('');
+
     const handleSubmit = (event) => {
         event.preventDefault();
     
         const name = nameRef.current.value;
         const surname = surnameRef.current.value;
     
-        console.log('Name:', name);
-        console.log('Surname:', surname);
+        if (!validateName(name)) {
+            setNameError('Please enter a valid name.');
+            return;
+        }
         
+        if (!validateSurname(surname)) {
+            setSurnameError('Please enter a valid surname.');
+            return;
+        }
+        
+        // Clear any existing error messages
+        setNameError('');
+        setSurnameError('');
+    
         setCreateData({Name:name, Surname:surname, Balance:0}); //su formos info sukuria nauja accounta
         setAddNewModalDisplay('none');
         // Reset the form
         nameRef.current.value = '';
         surnameRef.current.value = '';
 
-      };
+    };
+
+    const validateName = (name) => {
+    // Custom validation logic for name
+    return /^[A-Za-z\s]+$/.test(name);
+    };
+
+    const validateSurname = (surname) => {
+    // Custom validation logic for surname
+    return /^[A-Za-z\s]+$/.test(surname);
+    };
 
 
     function openAddNew (){
@@ -39,9 +63,11 @@ export default function AddNew({ setCreateData }){
                          
                         <label htmlFor='name'>Name:</label>
                         <input type="text" id="name" ref={nameRef}  required />
+                        {nameError && <div className="error-message">{nameError}</div>}
 
                         <label htmlFor='surname'>Surname:</label>
                         <input type="text" id="surname" ref={surnameRef} required />
+                        {surnameError && <div className="error-message">{surnameError}</div>}
 
                         <button className="btn" type="submit">Add account</button>
                     </form>
